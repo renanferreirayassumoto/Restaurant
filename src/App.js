09621ee4fce07e4ca4c5508de37e179ee5591ca5
Renan Ferreira Yassumoto <renankosmo@gmail.com>
2023-08-18 09:00:40 -0400

    teste

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div classname="topMenu">
-      <nav>
-        <ul>
-          <li>PEDIR MINHA PIZZA</li>
-          <li>CARDÁPIO</li>
-          <li>PROMOÇÕES</li>
-          <li>ACOMPANHAR PEDIDO</li>
-          <li><button>VEJA A DOMINO'S MAIS PRÓXIMA</button></li>
-          <li>FAZER LOGIN OU INICIAR CADASTRO</li>
-          <li>CARRINHO</li>
-        </ul>
-      </nav>
-    </div>
+    <></>
   );
 }
 
