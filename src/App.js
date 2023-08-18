4750448eecfec1934dbf49eb6b17f2fa1569ac4a
Renan Ferreira Yassumoto <renankosmo@gmail.com>
2023-08-18 08:55:24 -0400

    nav menu

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <></>
+    <div classname="topMenu">
+      <nav>
+        <ul>
+          <li>PEDIR MINHA PIZZA</li>
+          <li>CARDÁPIO</li>
+          <li>PROMOÇÕES</li>
+          <li>ACOMPANHAR PEDIDO</li>
+          <li><button>VEJA A DOMINO'S MAIS PRÓXIMA</button></li>
+          <li>FAZER LOGIN OU INICIAR CADASTRO</li>
+          <li>CARRINHO</li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
